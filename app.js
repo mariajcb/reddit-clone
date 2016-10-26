6d7cb4e9454ac84cfca767125d0eef4d4740fd50
mariajcb <bogomazm@gmail.com>
2016-10-26 14:46:15 -0600

    create color change for upvotes and downvotes

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.controller('DogController', function($scope) {
         author: "Bruiser W. Chihuahua",
         image: "https://static.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317-medium.jpeg",
         description: "Get a load of that dog!",
-        date: moment().subtract(1, 'days').calendar(),
+        date: new Date('December 17, 2016 03:24:00'),
+        dateMoment: moment().subtract(1, 'days').calendar(),
         votes: 4,
         comments: [{
             author: "Balto",
@@ -17,14 +18,13 @@ app.controller('DogController', function($scope) {
             author: "Willow Corgington the Marquess of Bath",
             text: "Cups are for tea. This is highly improper!"
         }],
-        commentsVisible: false,
-        newCommentVisible: false
     }, {
         title: "A+ Yawning",
         author: "Buddy Clinton",
         image: "https://static.pexels.com/photos/13869/a97c4a1614b53d6f55ae2464b06ba6aa-medium.jpg",
         description: "Get a load of that dog!",
-        date: moment().subtract(1, 'days').calendar(),
+        date: new Date('December 27, 2016 03:24:00'),
+        dateMoment: moment().subtract(1, 'days').calendar(),
         votes: 2,
         comments: []
     }, {
@@ -32,7 +32,8 @@ app.controller('DogController', function($scope) {
         author: "Willow Corgington the Marquess of Bath",
         image: "https://static.pexels.com/photos/164186/pexels-photo-164186-medium.jpeg",
         description: "Get a load of that dog!",
-        date: moment().subtract(1, 'days').calendar(),
+        date: new Date('December 21, 2016 03:24:00'),
+        dateMoment: moment().subtract(1, 'days').calendar(),
         votes: 3,
         comments: []
     }, {
@@ -40,7 +41,8 @@ app.controller('DogController', function($scope) {
         author: "Balto",
         image: "https://static.pexels.com/photos/60254/pexels-photo-60254-medium.jpeg",
         description: "Get a load of that dog!",
-        date: moment().subtract(1, 'days').calendar(),
+        date: new Date('December 7, 2016 03:24:00'),
+        dateMoment: moment().subtract(1, 'days').calendar(),
         votes: -1,
         comments: []
     }]
@@ -49,7 +51,7 @@ app.controller('DogController', function($scope) {
         event.preventDefault()
         if (post) {
             post.comments = []
-            post.date = moment().calendar()
+            post.dateMoment = moment().calendar()
             post.votes = 0
             $scope.view.posts.push($scope.view.post)
             $scope.post = ''
@@ -64,6 +66,16 @@ app.controller('DogController', function($scope) {
 
     $scope.minusOne = function(post) {
         post.votes -= 1
+    }
+
+    $scope.votes = function(post) {
+        if (post.votes > 0) {
+            return 'positive';
+        } else if (post.votes < 0) {
+            return 'negative';
+        } else {
+            return 'neutral';
+        }
     }
 
     //COMMENTS
@@ -82,5 +94,5 @@ app.controller('DogController', function($scope) {
     $scope.view.sort = ""
 
     //SEARCH
-    $scope.filter = ""
+    $scope.view.search = ""
 })
